167159381114f41486d820bafa8d123f4c7e5ae8
shimarrudz <vic.shima.vss@gmail.com>
2023-07-01 23:53:23 -0300

    feat: applying use case

--- modules/utils/functions.ts
+++ modules/utils/functions.ts
@@ -1,59 +1,67 @@
+// Aplicando métodos de Use Case.
 export function analyzePersonalData(serasaPFScore: number, foundationYears: number, serasaCompanyScore: number, annualRevenue: number, criminalModel: boolean, debtHistory: boolean): number {
-  let eligibilityPoints = 0;
-
   try {
-    if (serasaPFScore >= 700) {
-      eligibilityPoints += 30;
-    } else if (serasaPFScore >= 501) {
-      eligibilityPoints += 20;
-    } else if (serasaPFScore >= 301) {
-      eligibilityPoints += 10;
-    }
-  
-    if (foundationYears >= 10) {
-      eligibilityPoints += 15;
-    } else if (foundationYears >= 6) {
-      eligibilityPoints += 10;
-    } else if (foundationYears >= 3) {
-      eligibilityPoints += 5;
-    }
-  
-    if (serasaCompanyScore >= 700) {
-      eligibilityPoints += 30;
-    } else if (serasaCompanyScore >= 501) {
-      eligibilityPoints += 20;
-    } else if (serasaCompanyScore >= 301) {
-      eligibilityPoints += 10;
-    }
-  
-    if (annualRevenue >= 12000000) {
-      eligibilityPoints += 25;
-    } else if (annualRevenue >= 4800000) {
-      eligibilityPoints += 20;
-    } else if (annualRevenue >= 301000) {
-      eligibilityPoints += 10;
-    }
-  
-
-    if (!criminalModel) {
-      eligibilityPoints += 0;
-    } else {
-      eligibilityPoints = 0;
-    }
-  
-    if (!debtHistory) {
-      eligibilityPoints += 0;
-    } else {
-      eligibilityPoints = 0;
-    }
+    const eligibilityPoints = calculateEligibilityPoints(serasaPFScore, foundationYears, serasaCompanyScore, annualRevenue, criminalModel, debtHistory);
+    return clampEligibilityPoints(eligibilityPoints);
   } catch (error) {
-    // Tratamento de erros, se ocorrer algum problema nos cálculos
     console.error('Erro ao realizar a análise de elegibilidade:', error);
     throw new Error('Erro na análise de elegibilidade');
   }
+}
 
+function calculateEligibilityPoints(serasaPFScore: number, foundationYears: number, serasaCompanyScore: number, annualRevenue: number, criminalModel: boolean, debtHistory: boolean): number {
+  let eligibilityPoints = 0;
+
+  if (serasaPFScore >= 700) {
+    eligibilityPoints += 30;
+  } else if (serasaPFScore >= 501) {
+    eligibilityPoints += 20;
+  } else if (serasaPFScore >= 301) {
+    eligibilityPoints += 10;
+  }
+
+  if (foundationYears >= 10) {
+    eligibilityPoints += 15;
+  } else if (foundationYears >= 6) {
+    eligibilityPoints += 10;
+  } else if (foundationYears >= 3) {
+    eligibilityPoints += 5;
+  }
+
+  if (serasaCompanyScore >= 700) {
+    eligibilityPoints += 30;
+  } else if (serasaCompanyScore >= 501) {
+    eligibilityPoints += 20;
+  } else if (serasaCompanyScore >= 301) {
+    eligibilityPoints += 10;
+  }
+
+  if (annualRevenue >= 12000000) {
+    eligibilityPoints += 25;
+  } else if (annualRevenue >= 4800000) {
+    eligibilityPoints += 20;
+  } else if (annualRevenue >= 301000) {
+    eligibilityPoints += 10;
+  }
+
+  if (!criminalModel) {
+    eligibilityPoints += 0;
+  } else {
+    eligibilityPoints = 0;
+  }
+
+  if (!debtHistory) {
+    eligibilityPoints += 0;
+  } else {
+    eligibilityPoints = 0;
+  }
+
+  return eligibilityPoints;
+}
+
+function clampEligibilityPoints(eligibilityPoints: number): number {
   if (eligibilityPoints > 100) {
-    eligibilityPoints = 100;
+    return 100;
   }
 
   return eligibilityPoints;
